1868b0df9cea704c1904b0b226c5089f8e1aafe7
Fim1am <drozhanov@mail.ua>
2018-10-04 20:28:24 +0300

    Initial commit

---
+++ Assets/DBManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using UnityEngine;
+
+public class DBManager : MonoBehaviour
+{
+	private static string dbPath;
+	
+	void Start ()
+	{
+		dbPath = "URI = file://" + Application.dataPath + "/Highscores.sqlite";
+		GetScores();
+	}
+
+	public static void GetScores()
+	{
+		using (IDbConnection dbConnection = new SqlConnection(dbPath))
+		{
+			
+			dbConnection.Open();
+
+			using (IDbCommand dbCommand = dbConnection.CreateCommand())
+			{
+				
+				string sqlQuery = "select * from HS";
+		
+				dbCommand.CommandText = sqlQuery;
+
+				using (IDataReader reader = dbCommand.ExecuteReader())
+				{
+					
+					while (reader.Read())	
+					{
+						Debug.Log(reader.GetString(1) + " - " + reader.GetInt32(2));
+					}
+					
+					dbConnection.Close();
+					reader.Close();
+				}
+		
+		
+			}
+	
+		}
+		
+
+	}
+}
