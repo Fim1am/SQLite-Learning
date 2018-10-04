5a1186c3fc71768b20f94d916693b70b9aad435f
Fim1am <drozhanov@mail.ua>
2018-10-04 21:32:00 +0300

    gamefield

--- Assets/GameField.cs
+++ Assets/GameField.cs
@@ -2,15 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameField : MonoBehaviour {
+public class GameField : MonoBehaviour 
+{
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
